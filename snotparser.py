d1739825945d0ab73d78075fc7e7d9a35be46f8e
Max W Schwarz <wren@cat.pdx.edu>
2012-06-30 03:07:37 -0700

    Add line to detect bad tickets, and division between header, body, and footer

--- snotparser.py
+++ snotparser.py
@@ -7,7 +7,27 @@ ADMINISTRATIVE_START_MARKER = "XXXXXXXXXXXXXXXXXX  Administrative Information Fo
 def parseTicket(number):
     process = subprocess.Popen(['snot', '-sr', str(number)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     
-    rawTicket = process.stdout.readlines()
-    for index, line in enumerate(rawTicket):
-        print index, line,
+    bodyStartLine = 0
+    administrativeStartLine = 0
 
+
+    rawTicket = process.stdout.readlines()
+    #print rawTicket[0]
+    if "::::::::::::::" in rawTicket[0]: 
+        return "BAD TICKET"
+
+    enumeratedRawTicket = enumerate(rawTicket)
+    for index, line in enumeratedRawTicket:
+        if len(line) == 1:
+            bodyStartLine = index
+            break
+
+    for index, line in enumeratedRawTicket:
+        if ADMINISTRATIVE_START_MARKER in line:
+            administrativeStartLine = index
+            break
+    return (bodyStartLine, administrativeStartLine)
+    #return bodyStartLine
+
+for i in range(100000,110000):
+    print i, parseTicket(i)
